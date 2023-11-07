3e976cc9ba6815135ab7cc4ad8c0803a5f191ccb
Mateus Farias <mateusfariasdev@outlook.com>
2023-11-07 17:43:33 -0300

    validation

--- src/main/java/br/com/mfarias25/gestao_vagas/modules/candidate/CandiateEntity.java
+++ src/main/java/br/com/mfarias25/gestao_vagas/modules/candidate/CandiateEntity.java
@@ -1,8 +1,11 @@
 package br.com.mfarias25.gestao_vagas.modules.candidate;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
 
 import java.util.UUID;
 
@@ -10,9 +13,15 @@ import java.util.UUID;
 public class CandiateEntity {
 
     private UUID id;
+
+    @Pattern(regexp = "^(?!\\s*$).+", message = "O campo [username] não deve conter espaço")
     private String name;
+
+    @Email(message = "O campo [email] deve conter um email válido")
     private String username;
     private String email;
+
+    @Length (min = 10, max =100)
     private String password;
     private String description;
     private String curriculum;
--- src/main/java/br/com/mfarias25/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/br/com/mfarias25/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -1,6 +1,7 @@
 package br.com.mfarias25.gestao_vagas.modules.candidate.controllers;
 
 import br.com.mfarias25.gestao_vagas.modules.candidate.CandiateEntity;
+import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class CandidateController {
 
     @PostMapping("/")
-    public void create(@RequestBody CandiateEntity candidateEntity) {
+    public void create(@Valid @RequestBody CandiateEntity candidateEntity) {
         System.out.println("Candidato");
         System.out.println(candidateEntity.getEmail());
 
