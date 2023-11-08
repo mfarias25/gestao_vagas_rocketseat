b44372b7012f1944519084c60487ef8d49a7eca5
Mateus Farias <mateusfariasdev@outlook.com>
2023-11-08 16:01:24 -0300

    message validation password, username and email

--- src/main/java/br/com/mfarias25/gestao_vagas/modules/candidate/CandiateEntity.java
+++ src/main/java/br/com/mfarias25/gestao_vagas/modules/candidate/CandiateEntity.java
@@ -1,6 +1,7 @@
 package br.com.mfarias25.gestao_vagas.modules.candidate;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 import lombok.Getter;
@@ -14,14 +15,18 @@ public class CandiateEntity {
 
     private UUID id;
 
-    @Pattern(regexp = "^(?!\\s*$).+", message = "O campo [username] não deve conter espaço")
+
+    @NotBlank
     private String name;
 
-    @Email(message = "O campo [email] deve conter um email válido")
+    @NotBlank
+    @Pattern(regexp = "\\S+", message = "O campo [username] não deve conter espaço")
     private String username;
+
+    @Email(message = "O campo [email] deve conter um email válido")
     private String email;
 
-    @Length (min = 10, max =100)
+    @Length (min = 10, max =100, message = "A senha deve conter entre (10) e (100) caracteres")
     private String password;
     private String description;
     private String curriculum;
